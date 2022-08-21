5a7d5007ec1454286d36c47d361d53e1cf0059f9
Tomas Pilat <tomas.pilat@disig.sk>
2022-08-21 11:59:20 +0200

    added RunWithTaskAsync to HostExtensions

---
+++ src/Envelope.Hosting/Extensions/Microsoft/Extensions/Hosting/HostExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Envelope.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.Hosting;
+
+public static class HostExtensions
+{
+	public static async Task RunWithTasksAsync(this IHost host, CancellationToken cancellationToken = default)
+	{
+		var serviceProvider = host.Services;
+		var startupTasks = serviceProvider.GetServices<IStartupTask>();
+
+		using(var scope = serviceProvider.CreateScope())
+		{
+			var sp = scope.ServiceProvider;
+
+			foreach (var startupTask in startupTasks)
+				await startupTask.ExecuteAsync(sp, cancellationToken);
+		}
+
+		await host.RunAsync(cancellationToken);
+	}
+}
--- src/Envelope.Hosting/Extensions/ServiceCollectionExtensions.cs
+++ src/Envelope.Hosting/Extensions/ServiceCollectionExtensions.cs
@@ -9,20 +9,18 @@ public static partial class ServiceCollectionExtensions
 {
 	public static IServiceCollection AddHostApplicationContext(
 		this IServiceCollection services,
-		string systemName,
-		EnvelopePrincipal? principal)
-		=> services.AddScoped<IApplicationContext>(sp =>
-		{
-			var traceFrame = TraceFrame.Create();
-			var traceInfo = 
-				new TraceInfoBuilder(systemName, traceFrame, null)
-					.CorrelationId(Guid.NewGuid())
-					.ExternalCorrelationId(Guid.NewGuid().ToString("D"))
-					.Principal(principal)
-					.Build();
+		string systemName)
+		=> services.AddSingleton<IApplicationContext>(sp =>
+			{
+				var traceFrame = TraceFrame.Create();
+				var traceInfo = 
+					new TraceInfoBuilder(systemName, traceFrame, null)
+						.CorrelationId(Guid.NewGuid())
+						.ExternalCorrelationId(Guid.NewGuid().ToString("D"))
+						.Build();
 
-			var appResources = sp.GetRequiredService<IApplicationResources>();
-			var appCtx = new ApplicationContext(traceInfo, appResources, null);
-			return appCtx;
-		});
+				var appResources = sp.GetRequiredService<IApplicationResources>();
+				var appCtx = new ApplicationContext(traceInfo, appResources, null);
+				return appCtx;
+			});
 }
