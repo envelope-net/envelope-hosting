4e3a7b1cb4c788579f2216bd1d8dada2e07fe511
Tomas Pilat <tomas.pilat@disig.sk>
2022-09-01 17:50:56 +0200

    scoped IApplicationContext

--- src/Envelope.Hosting/Extensions/ServiceCollectionExtensions.cs
+++ src/Envelope.Hosting/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using Envelope.Localization;
 using Envelope.Trace;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Envelope.Hosting.Extensions;
 
@@ -10,17 +11,21 @@ public static partial class ServiceCollectionExtensions
 	public static IServiceCollection AddHostApplicationContext(
 		this IServiceCollection services,
 		string systemName)
-		=> services.AddSingleton<IApplicationContext>(sp =>
-			{
-				var traceFrame = TraceFrame.Create();
-				var traceInfo = 
-					new TraceInfoBuilder(systemName, traceFrame, null)
-						.CorrelationId(Guid.NewGuid())
-						.ExternalCorrelationId(Guid.NewGuid().ToString("D"))
-						.Build();
+	{
+		services.TryAddScoped<IApplicationContext>(sp =>
+		{
+			var traceFrame = TraceFrame.Create();
+			var traceInfo =
+				new TraceInfoBuilder(systemName, traceFrame, null)
+					.CorrelationId(Guid.NewGuid())
+					.ExternalCorrelationId(Guid.NewGuid().ToString("D"))
+					.Build();
 
-				var appResources = sp.GetRequiredService<IApplicationResources>();
-				var appCtx = new ApplicationContext(traceInfo, appResources, null);
-				return appCtx;
-			});
+			var appResources = sp.GetRequiredService<IApplicationResources>();
+			var appCtx = new ApplicationContext(traceInfo, appResources, null);
+			return appCtx;
+		});
+
+		return services;
+	}
 }
